fd1408650e2cd7be08b758995b8987f9555d571b
Sam Dutton <samdutton@gmail.com>
2013-10-15 09:20:10 +0100

    First commit

---
+++ slide_config.js
@@ -0,0 +1,25 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Chrome Dev Rel',
+    subtitle: 'GDG Leads, London 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    analytics: 'UA-2630780-1',
+    favIcon: 'images/chrome-logo-tiny2.png',
+    // fonts: ["Open Sans"],
+    theme: ['sam'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Sam Dutton',
+    company: 'Developer Advocate, Google Chrome',
+    gplus: 'plus.samdutton.com',
+    twitter: '@sw12',
+    www: 'http://www.samdutton.com'
+  }]
+};
+
